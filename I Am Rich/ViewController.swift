18861be867359f89f8a96b4842931eef17717d6a
Sandra <sandraxdjurovic@gmail.com>
2019-02-28 21:00:37 -0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Sandra Durovic on 2/28/19.
+//  Copyright © 2019 Sandra Durovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
